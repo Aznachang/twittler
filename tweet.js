e1466d2c3b8caba03df0bc03f99bebe0c1e5c852
Albert Chang <aznachang@gmail.com>
2016-10-15 17:45:54 -0700

    Can update BOTH 'all tweets' and 'specific-user tweets'

--- tweet.js
+++ tweet.js
@@ -1,60 +1,65 @@
 $(document).ready(function(){
-
+  var tweetHandle = '';
   var displayTweets = function(tweetHandle) {
-
-  // Displays all tweets - checks to see if it is a valid username handle
-  if(tweetHandle === '') {
-    //$('.user-twitts-container').hide();
-    $('.all-twitts').html('');
-    console.log("Display All Tweets");
-  /********* Display ALL Tweets *********/
-
-  //<div class = "all-twitts">
-    //<div class = "user shawndrost">@shawndrost:</div>
-    //<div class = "message">some message</div>
-    //<div class = "date"> a few seconds ago</div>
-    // </div><hr>
-
-    streams.home.forEach(function(tweet) {
-      $('.all-twitts').prepend('<div class="tweet"><div class="user ' +
-        tweet.user + '">@' + tweet.user +
-        ':</div><div class="message">' + tweet.message +
-        '</div><div class="date">' + moment(tweet.created_at).fromNow() +
-         "</div></div><hr>");
-      });
-
-        //Display tweets by clicking on specific-username
-    $('.shawndrost').on('click', function() {
-        displayTweets('shawndrost');
-    });
-
-    $('.sharksforcheap').on('click', function() {
-       displayTweets('sharksforcheap');
-    });
-
-    $('.mracus').on('click', function() {
-      displayTweets('mracus');
-    });
-
-    $('.douglascalhoun').on('click', function() {
-      displayTweets('douglascalhoun');
-    });
-
+    //Check for New Tweets every 2 seconds
     setTimeout(function(){
       displayTweets(tweetHandle);
-    }, 2000);
+    }, 500);
 
-  } //end of if
+    // Displays all tweets - checks to see if it is a valid username handle
+    if(tweetHandle === '') {
+      //$('.user-twitts-container').hide();
+      $('.all-twitts').html('');
+      console.log("Display All Tweets");
+    /********* Display ALL Tweets *********/
+
+    //<div class = "all-twitts">
+      //<div class = "user shawndrost">@shawndrost:</div>
+      //<div class = "message">some message</div>
+      //<div class = "date"> a few seconds ago</div>
+      // </div><hr>
+
+      streams.home.forEach(function(tweet) {
+        $('.all-twitts').prepend('<div class="tweet"><div class="user ' +
+          tweet.user + '">@' + tweet.user +
+          ':</div><div class="message">' + tweet.message +
+          '</div><div class="date">' + moment(tweet.created_at).fromNow() +
+           "</div></div><hr>");
+        });
+
+          //Display tweets by clicking on specific-username
+      $('.shawndrost').on('click', function() {
+          tweetHandle = 'shawndrost';
+         // displayTweets('shawndrost');
+      });
+
+      $('.sharksforcheap').on('click', function() {
+         tweetHandle = 'sharksforcheap';
+        // displayTweets('sharksforcheap');
+      });
+
+      $('.mracus').on('click', function() {
+        tweetHandle = 'mracus';
+       // displayTweets('mracus');
+      });
+
+      $('.douglascalhoun').on('click', function() {
+        tweetHandle = 'douglascalhoun';
+        //displayTweets('douglascalhoun');
+      });
+
+    } //end of if
 
 /************ Display specific user tweets  ***********/
-    else {
-     // console.log("Display Specific User Tweets!");
+    else{
+     //**** GO BACK TO HOME PAGE - Upon 'View All' Button-Click ****/
       $('.btn-primary').on('click', function() {
+
         $('.user-twitts-container').fadeOut(function() {
           $('.user-twitts-container').hide();
         });
-
         $('.all-twitts-container').fadeIn();
+        tweetHandle = '';
       });
 
       $('.all-twitts-container').fadeOut(function() {
@@ -73,11 +78,12 @@ $(document).ready(function(){
           '</div><div class="date">' + moment(tweet.created_at).fromNow() +
           "</div></div><hr>");
       });
-    }; //end of else
 
-  } //end of displayTweets()
+    } //end of else
+
+  }; //end of displayTweets()
 
   //'Display and Refresh' ALL TWEETS on HOME PAGE
-  displayTweets('');
+  displayTweets(tweetHandle);
 
 }); //end of file
